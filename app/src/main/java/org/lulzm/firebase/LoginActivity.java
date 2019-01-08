4174362b57a8087d578d7ce8862cbcc090ec23f2
scadasystems <yunmo159789@gmail.com>
2019-01-08 11:47:18 +0900

    Firebase 로그인 기능 연습 --2019-01-08 ---Edit-by-Lulz_M

---
+++ app/src/main/java/org/lulzm/firebase/LoginActivity.java
@@ -0,0 +1,112 @@
+package org.lulzm.firebase;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonSignin;
+    TextView textviewSingin;
+    TextView textviewMessage;
+    TextView textviewFindPassword;
+    ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            //이미 로그인 되었다면 이 액티비티를 종료함
+            finish();
+            //그리고 profile 액티비티를 연다.
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textviewSingin = (TextView) findViewById(R.id.textViewSignin);
+        textviewMessage = (TextView) findViewById(R.id.textviewMessage);
+        textviewFindPassword = (TextView) findViewById(R.id.textViewFindpassword);
+        buttonSignin = (Button) findViewById(R.id.buttonSignup);
+        progressDialog = new ProgressDialog(this);
+
+        //button click event
+        buttonSignin.setOnClickListener(this);
+        textviewSingin.setOnClickListener(this);
+        textviewFindPassword.setOnClickListener(this);
+
+    }
+
+    //firebase userLogin method
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("로그인중입니다. 잠시 기다려 주세요...");
+        progressDialog.show();
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "로그인 실패!", Toast.LENGTH_LONG).show();
+                            textviewMessage.setText("로그인 실패 유형\n - password가 맞지 않습니다.");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignin) {
+            userLogin();
+        }
+        // 신규 회원가입 누르면 이동.
+        if (view == textviewSingin) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+        // 패스워드 찾기
+        if (view == textviewFindPassword) {
+            startActivity(new Intent(LoginActivity.this, FindActivity.class));
+            finish();
+        }
+    }
+}
